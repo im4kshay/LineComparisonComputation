2d8b52555d40b820584bbcb87e44d20f702778a9
im4kshay <akshaysayre7@gmail.com>
2022-03-30 23:56:01 +0530

    Calculate length of a line

---
+++ LineComparisonComputationProgram/LineComparison.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineComparisonComputationProgram
+{
+    class LineComparison
+    {
+        public double xAxisStart, yAxisStart, xAxisEnd, yAxisEnd;
+        public void PointValues()
+        {
+            Console.WriteLine("Enter X Start");
+            xAxisStart = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter Y Start");
+            yAxisStart = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter X End");
+            xAxisEnd = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter Y End");
+            yAxisEnd = Convert.ToDouble(Console.ReadLine());
+        }
+
+        public double GetLength()
+        {
+            double length = Math.Sqrt(Math.Pow((xAxisEnd - xAxisStart), 2) + Math.Pow((yAxisEnd - yAxisStart), 2));
+            return length;
+        }
+    }
+}
